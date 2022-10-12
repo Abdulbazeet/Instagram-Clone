54dc99390c750edf66de25ddc844fc9bd97d123e
Abdulbazeet <abdulbazeetbalogun4@gmail.com>
2022-10-12 17:49:49 +0100

    next comit

--- lib/screens/mobile/mobile_screen.dart
+++ lib/screens/mobile/mobile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagramclone/const/colors.dart';
 import 'package:instagramclone/provider/user_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -12,24 +14,41 @@ class MobileScreenSize extends StatefulWidget {
 }
 
 class _MobileScreenSizeState extends State<MobileScreenSize> {
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     addData();
   }
+
   addData() async {
     UserProvider _userProvider = Provider.of(context, listen: false);
     await _userProvider.refreshUser();
   }
+
   @override
   Widget build(BuildContext context) {
-    Users users = Provider.of<UserProvider>(context).getUser;
-    return  Scaffold(
-      body: Center(
-        child: Text(users.username),
+    // Users users = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
+      body: const Center(
+        child: Text("I discovered it "),
       ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home), label: "", backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search), label: "", backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home), label: "", backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle), label: "", backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite), label: "", backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person), label: "", backgroundColor: primaryColor)
+      ]),
     );
   }
 }
